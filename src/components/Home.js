6f104e3bb1af4059ae76f54f245e0a397531d8d9
Himanshu Soni <63811466+L00kAhead@users.noreply.github.com>
2022-02-05 22:44:22 +0700

    build the home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React from "react";
 import styled from "styled-components";
-import Section from "./Section.js"
+import Section from "./Section.js";
+import carInfo from "../data/carInfo.json";
 const Home = () => {
+  const data = carInfo;
+  console.log(data);
   return (
-      <Container>
-          <Section />
-      </Container>
+    <Container>
+      {data.map((carData) => {
+        return (
+          <Section
+            key={carData.id}
+            id={carData.id}
+            title={carData.title}
+            description={carData.description}
+            backgroundImage={carData.image}
+            range={carData.range}
+            time={carData.time}
+            topSpeed={carData.topSpeed}
+            peakPower={carData.peakPower}
+            leftBtnText = {carData.leftBtnText}
+            rightBtnText = {carData.rightBtnText}
+          />
+        );
+      })}
+    </Container>
   );
 };
 
 export default Home;
 
 const Container = styled.div`
-    height: 100vh;
-
-`
+  height: 100vh;
+`;
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,17 +1,20 @@
 import React from "react";
 import styled from "styled-components";
-const Section = () => {
-  return (
-    <Wrap>
-      <ItemText>
-        <h1>Model S</h1>
-        <p>Order Online for Touchless Delivery</p>
-      </ItemText>
+const Section = (props) => {
 
-      <ButtonGroup>
-        <LeftButton>Custom Order</LeftButton>
-        <RightButton>Existing Inventory</RightButton>
-      </ButtonGroup>
+  return (
+    <Wrap bgImg={props.backgroundImage}>
+      <ItemText>
+        <h1>{props.title}</h1>
+        <p>{props.description}</p>
+      </ItemText>
+      <Buttons>
+        <ButtonGroup>
+          <LeftButton>{props.leftBtnText}</LeftButton>
+          {props.rightBtnText && <RightButton>{props.rightBtnText}</RightButton>}
+        </ButtonGroup>
+        <DownArrow src="./images/down-arrow.svg" />
+      </Buttons>
     </Wrap>
   );
 };
@@ -24,7 +27,11 @@ const Wrap = styled.div`
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
-  background-image: url("./images/model-s.jpg");
+  background-image: ${props => `url("/images/${props.bgImg}")`};
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 const ItemText = styled.div`
@@ -32,8 +39,43 @@ const ItemText = styled.div`
   text-align: center;
 `;
 
-const ButtonGroup = styled.div``;
+const ButtonGroup = styled.div`
+  display: flex;
+  margin-bottom: 5rem;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
 const LeftButton = styled.div`
   background-color: rgba(23, 26, 32, 0.8);
+  height: 2.5rem;
+  width: 16rem;
+  color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 6.5rem;
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 0.75rem;
+  cursor: pointer;
+  margin: 1rem;
 `;
-const RightButton = styled(LeftButton)``;
+
+const RightButton = styled(LeftButton)`
+  background-color: #fff;
+  opacity: 0.65;
+  color: #000;
+`;
+
+const DownArrow = styled.img`
+  height: 2.5rem;
+  animation: animateDown infinite 1.5s;
+  margin-left: 16rem;
+  @media (max-width: 768px) {
+    margin-left: 8rem;
+  }
+`;
+
+const Buttons = styled.div``;
